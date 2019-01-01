4976e86a9f280d03cceb9e56cbac0714edcdc58b
jorgechavezrnd <jorgechavezrnd@gmail.com>
2018-12-31 20:17:49 -0400

    students details terminado

--- lib/studentsDetails.dart
+++ lib/studentsDetails.dart
@@ -11,7 +11,15 @@ class StudentsDetails extends StatefulWidget {
   
 }
 
-class _StudentsDetailsState extends State<StudentsDetails> {
+class _StudentsDetailsState extends State<StudentsDetails> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+    void initState() {
+      super.initState();
+      _tabController = new TabController(vsync: this, initialIndex: 1, length: 4);
+    }
+
   @override
   Widget build(BuildContext context) {
     print(widget.response);
@@ -24,6 +32,20 @@ class _StudentsDetailsState extends State<StudentsDetails> {
                 color: Colors.white,
                 fontSize: 14.0,
                 fontWeight: FontWeight.bold)),
+        bottom: new TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            new Tab(icon: new Icon(Icons.camera_alt)),
+            new Tab(text: "CHATS"),
+            new Tab(
+              text: "STATUS",
+            ),
+            new Tab(
+              text: "CALLS",
+            )
+          ],
+        ),
       ),
       body: Text(widget.response.toString())
     );
